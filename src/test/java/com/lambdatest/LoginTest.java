a1bd2275361ec3fec112007b5e0d359c7f1d2711
skatikireddy <vasu.0018@gmail.com>
2023-08-20 12:47:19 +0530

    Added couple of test cases

---
+++ src/test/java/com/lambdatest/LoginTest.java
@@ -0,0 +1,114 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class LoginTest {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        
+        String username = "srinivas.kishafoundation";
+        String authkey = "MCtpqmcJj7B6NJfj38NAtD5eYW6UUgwXgF77zqNAMhY1mkbEEI";
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "Windows 11");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "latest");
+        caps.setCapability("build", "TestNG With Java_Jenkins126");
+        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        caps.setCapability("console", true);
+        caps.setCapability("terminal", true);
+        String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+
+    }
+    
+
+    @Test
+	public void verifyLogin() {
+	// declaration and instantiation of objects/variables  
+        //System.setProperty("webdriver.chrome.driver", "/home/isha/Selenium/chromedriver-linux64/chromedriver");  
+        //WebDriver driver=new ChromeDriver();
+        
+     // 2 | setWindowSize | 1366x729 |  | 
+        //driver.manage().window().setSize(new Dimension(1366, 729));
+        driver.manage().window().maximize();
+        
+    	// Test name: Login_Testcase
+        // Step # | name | target | value | comment
+        // 1 | open | / |  | 
+        driver.get("https://isha.sadhguru.org/");
+        
+        // 3 | assertElementPresent | css=.css-131wcj4 > .chakra-image |  | 
+        {
+          List<WebElement> elements = driver.findElements(By.cssSelector(".css-131wcj4 > .chakra-image"));
+          assert(elements.size() > 0);
+        }
+        // 4 | clickAt | xpath=//*[@id="__next"]/div/div[1]/header/div[2]/div/div[1]/img |  | 
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[1]/header/div[2]/div/div[1]/img")).click();
+        // 5 | click | linkText=Login |  | 
+        driver.findElement(By.linkText("Login")).click();
+        // 6 | click | css=#withEmail > .head |  | 
+        
+        WebElement firstResult = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[4]/div[3]/section/div/div/div/div/div/div[1]/div/div[9]/div/div/div/div/a/div[2]")));
+        // Print the first result
+        System.out.println(firstResult.getText());
+        
+        
+        /*if(driver.findElement(By.xpath("//*[@id=\\\"withEmail\\\"]/div[2]")).isDisplayed())
+        {
+        driver.findElement(By.xpath("//*[@id=\"withEmail\"]/div[2]")).click();
+        }
+        else
+        	System.out.println("'Login with Email ID' field not present");*/
+        // 7 | click | id=txtEmail |  | 
+        driver.findElement(By.xpath("/html/body/div[4]/div[3]/section/div/div/div/div/div/div[1]/div/div[9]/div/div/div/div/a/div[2]")).click();
+        // 8 | type | id=txtEmail | vasu.0018@gmail.com | 
+        driver.findElement(By.id("txtEmail")).sendKeys("email@gmail.com");
+        // 9 | click | id=txtPassword |  | 
+        driver.findElement(By.id("txtPassword")).click();
+        // 10 | type | id=txtPassword | r3st@rtl!f3 | 
+        driver.findElement(By.id("txtPassword")).sendKeys("password");
+        // 11 | click | id=btnLogin |  | 
+        driver.findElement(By.id("btnLogin")).click();
+        
+			System.out.println("User Loged out SUCCESSFULL");
+			System.out.println("Login Test SUCCESSFUL");
+		
+        driver.close();
+        //driver.quit();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/com/lambdatest/SampleCookieTest.java
@@ -0,0 +1,128 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class SampleCookieTest {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        
+        String username = "srinivas.kishafoundation";
+        String authkey = "MCtpqmcJj7B6NJfj38NAtD5eYW6UUgwXgF77zqNAMhY1mkbEEI";
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "Windows 11");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "latest");
+        caps.setCapability("build", "TestNG With Java_Jenkins126");
+        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        caps.setCapability("console", true);
+        caps.setCapability("terminal", true);
+        String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+
+    }
+    
+   @Test
+    public void sampleCookieVerification() {
+    	// declaration and instantiation of objects/variables  
+            //System.setProperty("webdriver.chrome.driver", "/home/isha/Selenium/chromedriver-linux64/chromedriver");  
+            //WebDriver driver=new ChromeDriver();
+            driver.manage().window().maximize();
+            
+            driver.get("https://isha.sadhguru.org/uk/en");
+          //get all the cookies
+        	Set <Cookie> cookies = null;
+        	try {
+    			Thread.sleep(3000);
+    		} catch (InterruptedException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        	//cookies.clear();
+        	cookies = driver.manage().getCookies();
+        	
+        	try {
+    			Thread.sleep(5000);
+    		} catch (InterruptedException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        		
+        	//print the size of the cookies
+        	System.out.println("Total number of cookies Before deletion is " + cookies.size());
+        	
+        	/*for(Cookie cookie : cookies) {
+        		System.out.println(cookie.getName() + ":" + cookie.getValue());
+        	}*/
+        	
+        	if(driver.manage().getCookieNamed("bottom_widget") != null)
+        	{
+        		System.out.println("Cookie Bottom Widget is Present");
+        	}
+        	else
+        		System.out.println("Cookie Bottom Widget does not Exists");
+        	
+            driver.manage().deleteCookieNamed("bottom_widget");
+            try {
+    			Thread.sleep(3000);
+    		} catch (InterruptedException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+          //cookies.clear();
+        	cookies = driver.manage().getCookies();
+        		
+        	//print the size of the cookies
+        	System.out.println("Total number of cookies After deletion is " + cookies.size());
+        	
+        	/*for(Cookie cookie : cookies) {
+        		System.out.println(cookie.getName() + ":" + cookie.getValue());
+        	}*/
+        	
+        	if(driver.manage().getCookieNamed("bottom_widget") != null)
+        	{
+        		System.out.println("Cookie 'Bottom Widget' not Deleted");
+        	}
+        	else {
+        		System.out.println("SUCCESSFULLY, Cookie 'Bottom Widget' got deleted.");
+        	System.out.println("Cookie Deletion test passed");
+        	}
+        	driver.close();
+        	driver.quit();
+        }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
